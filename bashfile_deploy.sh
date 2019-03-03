1143d100b2b9701859924c9bd13ef6d55e6d26a7
phamphuc1301 <phamphuc224@gmail.com>
2019-03-04 01:49:42 +0700

    init

---
+++ bashfile_deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+text1="<groupId>org.codehaus.mojo</groupId>"
+echo "Hi give me a branch ><"
+cd /opt/source-code/intellivu
+current_branch=`git branch | awk '/\*/ { print $2; }'`
+echo current branch is: $current_branch
+echo Enter branch name to deploy
+read branch
+if $current_branch=:$branch; then
+    git pull
+else
+    stringPomWeb=`grep -n "<groupId>org.codehaus.mojo</groupId>" intelli-vu/pom.xml | xargs echo | cut -d ":" -f 1`
+    stringPomEvent=`grep -n "<groupId>org.codehaus.mojo</groupId>" intelli-vu-event/pom.xml | xargs echo | cut -d ":" -f 1`
+    numberLinePomWeb=`expr $stringPomWeb - 2`
+    numberLineEndPomWeb=`expr $numberLinePomWeb + 47`
+    numberLinePomEvent=`expr $stringPomEvent - 2`
+    numberLineEndPomEvent=`expr $numberLinePomEvent + 47`
+    echo checkout source code
+    git reset --hard
+    git checkout $branch
+    git pull
+    characterOpenComment='i<!--'
+    characterCloseComment='i-->'
+    echo edit pom file
+    sed -i "$numberLinePomWeb$characterOpenComment" intelli-vu/pom.xml
+    sed -i "$numberLineEndPomWeb$characterCloseComment" intelli-vu/pom.xml
+    sed -i "$numberLinePomEvent$characterOpenComment" intelli-vu-event/pom.xml
+    sed -i "$numberLineEndPomEvent$characterCloseComment" intelli-vu-event/pom.xml
+fi
+    mvn clean install -DskipTests
+    echo coppy files
+    scp intelli-vu/target/intelli-vu.war /usr/share/tomcat/webapps
+    scp intelli-vu-event/target/intelli-vu-event.war .ssh hnguyen2@usla-ivev-q001.qa.vubiquity.com:./
+
+    cd /usr/share/tomcat/bin
+    killall java
+    ./startup.sh
+    tail -f ../logs/catalila.out
